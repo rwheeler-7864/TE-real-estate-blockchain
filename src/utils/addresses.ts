c0c679112f0e8e271b60d26a62ea304d22d98fcc
Justin Naismith s3605206 <s3605206@student.rmit.edu.au>
2021-06-03 10:07:06 +1000

    fixes and comments

---
+++ src/utils/addresses.ts
@@ -0,0 +1,9 @@
+// Justin's addresses
+
+export const Authority = '0x2b0d61c05D8caFF492E1d6a6D3451437801D4b6B';
+export const Seller = '0x7e14E1b59aF858C2E366B1d902aBBB743275F694';
+
+// export const Buyer = ;
+// export const Bank = ;
+
+// Siddhesh's addresses
