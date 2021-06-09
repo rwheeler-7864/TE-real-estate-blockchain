e2d5e5e249efb8790829275a2114233db11c8e00
Siddhesh Kale s3854666 <s3854666@student.rmit.edu.au>
2021-06-09 20:55:15 +1000

    Added sid's addresses

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -75,16 +75,16 @@ contract Marketplace {
     name = 'SICI Real Estate Marketplace';
 
     // Justins addresses
-    seller = 0x7e14E1b59aF858C2E366B1d902aBBB743275F694;
-    authority = 0x2b0d61c05D8caFF492E1d6a6D3451437801D4b6B;
-    buyer = 0x457e9ddFf1409434D618461a2f5F15586FB0eb6C;
-    bank = 0xFD55579F56578959D269f87D9eAB9A97A09877f8;
+    // seller = 0x7e14E1b59aF858C2E366B1d902aBBB743275F694;
+    // authority = 0x2b0d61c05D8caFF492E1d6a6D3451437801D4b6B;
+    // buyer = 0x457e9ddFf1409434D618461a2f5F15586FB0eb6C;
+    // bank = 0xFD55579F56578959D269f87D9eAB9A97A09877f8;
 
     // Sids addresses
-    // seller = <ADD>;
-    // authority = <ADD>;
-    // buyer = <ADD>;
-    // bank = <ADD>;
+    seller = 0x386c0b9C66a334cEedf0059b1B4E44E43C2821a6;
+    authority = 0x2E11fF485F0B543222e5cb392395f3ec748E37B8;
+    buyer = 0x728Ad52C853e97Bb907F83F842043567266e3483;
+    bank = 0x5c5857f8cEB15DA1DF8d0217EcA61c8B19db501E;
   }
 
   function createPermit(
